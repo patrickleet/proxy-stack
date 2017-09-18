d864013ca692a83377b8e38af460e504f6a05069
Patrick Scott <patrick.scott@zx-ventures.com>
2017-09-18 16:13:49 -0400

    update for d4aws

--- init.sh
+++ init.sh
@@ -1,2 +1,34 @@
 docker network create --driver overlay proxy
 docker network create --driver overlay monitor
+
+echo "route:
+  group_by: [service,scale]
+  repeat_interval: 5m
+  group_interval: 5m
+  receiver: 'slack'
+  routes:
+  - match:
+      service: 'go-demo_main'
+      scale: 'up'
+    receiver: 'jenkins-go-demo_main-up'
+  - match:
+      service: 'go-demo_main'
+      scale: 'down'
+    receiver: 'jenkins-go-demo_main-down'
+receivers:
+  - name: 'slack'
+    slack_configs:
+      - send_resolved: true
+        title: '[{{ .Status | toUpper }}] {{ .GroupLabels.service }} service is in danger!'
+        title_link: 'http://$CLUSTER_DNS/monitor/alerts'
+        text: '{{ .CommonAnnotations.summary}}'
+        api_url: 'https://hooks.slack.com/services/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'
+  - name: 'jenkins-go-demo_main-up'
+    webhook_configs:
+      - send_resolved: false
+        url: 'http://$CLUSTER_DNS/jenkins/job/service-scale/buildWithParameters?token=DevOps22&service=go-demo_main&scale=1'
+  - name: 'jenkins-go-demo_main-down'
+    webhook_configs:
+      - send_resolved: false
+        url: 'http://$CLUSTER_DNS/jenkins/job/service-scale/buildWithParameters?token=DevOps22&service=go-demo_main&scale=-1'
+" | docker secret create alert_manager_config -
